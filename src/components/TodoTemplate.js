52853a4ff74fab1202a176aaadf944e7d1f868ad
김지호 <jiho001108@gmail.com>
2024-04-18 22:20:30 +0900

    TodoTemplate.scss

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -3,8 +3,8 @@ import './TodoTemplate.scss';
 const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
-      <div>일정 관리</div>
-      <div>{children}</div>
+      <div className="app-title">일정 관리</div>
+      <div className="content">{children}</div>
     </div>
   );
 };
