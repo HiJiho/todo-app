80385fd2c6ef30dfcc8d208a4036a46aea3d41a7
김지호 <jiho001108@gmail.com>
2024-04-19 16:38:53 +0900

    todo props in TodoListItem & styling

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -4,13 +4,17 @@ import {
   MdRemoveCircleOutline,
 } from 'react-icons/md';
 import './TodoListItem.scss';
+import cn from 'classnames';
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo }) => {
+  // key를 따로 처리하기에 받아오지 않음
+  const { text, checked } = todo;
+
   return (
     <div className="TodoListItem">
-      <div className="checkbox">
-        <MdCheckBoxOutlineBlank />
-        <div className="text">할 일</div>
+      <div className={cn('checkbox', { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
       </div>
       <div className="remove">
         <MdRemoveCircleOutline />
