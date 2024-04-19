67b20b8620ffca655be120298f1e176e5319e878
김지호 <jiho001108@gmail.com>
2024-04-19 17:23:34 +0900

    todos 배열에 새 객체 추가하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -22,9 +22,27 @@ const App = () => {
     },
   ]);
 
+  // 고윳값으로 사용될 id
+  // ref를 사용하여 변수 담기, 렌더링에 영향받지 않는 로컬변수!!
+  const nextId = useRef(4);
+
+  //? 어떻게 TodoInsert의 state를 App에서 사용할까?
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1; // nextId 1씩 더하기
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos} />
     </TodoTemplate>
   );
