20df605668a247fc03c2e4b239ab97be00d947f6
김지호 <jiho001108@gmail.com>
2024-04-19 11:17:45 +0900

    TodoListItem UI

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,9 +1,20 @@
+import {
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
 import './TodoListItem.scss';
 
 const TodoListItem = () => {
   return (
-    <div>
-      
+    <div className="TodoListItem">
+      <div className="checkbox">
+        <MdCheckBoxOutlineBlank />
+        <div className="text">할 일</div>
+      </div>
+      <div className="remove">
+        <MdRemoveCircleOutline />
+      </div>
     </div>
   );
 };
